c9b83f675005ed93b601307d2dd09d9ff854adcf
Nathan Looney <looney@arqspin.com>
2015-12-08 12:58:54 -0500

    Program reads in input files, creates pieces, outputs them

--- index.py
+++ index.py
@@ -1,34 +1,60 @@
-
-
-def buildpiece(array, r, c):
-	traverse = [[-1,0],[0,-1],[0,1],[1,0]]
-	x = 0
-	y = 0
-
-	piece = []
+from collections import deque
+from piece import Piece
 
 
 
-def checkRight(array, r, c, piece, x, y):
-	if " " not in array[r][c+1]:
+# List to store the info of each piece.
+pieceCollection = []
+#List to store each piece represented as a matrix.
+constructedCollection = []
 
 
 
-def checkLeft(array, r, c):
 
-
-def checkUp(array, r, c):
-
+def checkRight(array, r, c):
+	try:
+		if " " not in array[r][c+1]:
+			return True
+		else:
+			return False
+	except:
+		return False
 
 def checkDown(array, r, c):
+	try:
+		if " " not in array[r+1][c]:
+			return True
+		else:
+			return False
+	except:
+		return False
+
+def checkLeft(array, r, c):
+	try:
+		if " " not in array[r][c-1]:
+			return True
+		else:
+			return False
+	except:
+		return False
+
+def checkUp(array, r, c):
+	try:
+		if " " not in array[r-1][c]:
+			return True
+		else:
+			return False
+	except:
+		return False
 
 
-def main():
 
+#Reads in the text file, adds each character (including spaces) to an array, if it finds a new line it creates a new row.
+#Basically lays out the text file as a grid for easier processing.
+def textFileToArray():
 	rowcount = 0
+	array = [[]]	
 	with open("test.txt") as f:
-		array = []
-		array.append([])
 		for line in f:
 			for char in line:
 				if "\n" in char:
@@ -36,11 +62,63 @@ def main():
 					array.append([])
 					continue
 				array[rowcount].append(char)
+	return array
 
-	buildpiece(array, 2, 5)
-	for row in array:
-		print row
+
+#Starts searching through the array, once it finds a brick it starts to find the entire thing using findPiece (read comment for findPiece)
+def traverseArray(array):
+	for i in range(0, len(array)-1):
+		for j in range (len(array[i])-1):
+			if " " not in array[i][j]:
+				array = findPiece(array, i, j)
+	return array
+
+
+# When traverse array finds a "brick", it sends the value and location to this function which uses Connected Component Labeling (wiki)
+# to explore surrounding bricks to find all those connected using a queue.
+# This is basically how breadth first search works
+def findPiece(array, r, c):
+	brickList = []
+	queue = deque([])
+	queue.append([[r,c], array[r][c]])
+	#If a brick is found, it's removed from the array so that it isn't found again later.
+	array[r][c] = " "
+
+	while len(queue) > 0:
+		brick = queue.popleft()
+		x = brick[0][0]
+		y = brick[0][1]
+		brickList.append(brick)
+
+		if checkRight(array, x, y):
+			queue.append([[x,y+1], array[x][y+1]])
+			array[x][y+1] = " "
+		if checkDown(array, x, y):
+			queue.append([[x+1,y], array[x+1][y]])
+			array[x+1][y] = " "
+		if checkLeft(array, x, y):
+			queue.append([[x,y-1], array[x][y-1]])
+			array[x][y-1] = " "
+		if checkUp(array, x, y):
+			queue.append([[x-1,y], array[x-1][y]])
+			array[x-1][y] = " "
+
+	currentPiece = Piece(brickList)
+	pieceCollection.append(currentPiece)
+	return array
+
+
 
 
 if __name__ == "__main__":
-	main()
+	array = textFileToArray()
+	array = traverseArray(array)
+	for pieces in pieceCollection:
+		print pieces.height,
+		print "x",
+		print pieces.width
+		for rows in pieces.matrix:
+			print rows
+		print "\n"
+
+
---
+++ piece.py
@@ -0,0 +1,40 @@
+class Piece:
+
+
+	def __init__(self, inputBrickList):
+		self.brickList = inputBrickList
+		self.constructMatrix()
+
+
+	#Goes through passed in list of bricks and creates the matrix
+	def constructMatrix(self):
+			dimensions = self.getDimensions()
+			minWidth = dimensions[0]
+			minHeight = dimensions[1]
+			self.matrix = [[" " for x in range(0, self.width)] for x in range(0, self.height)]
+			for bricks in self.brickList:
+				self.matrix[bricks[0][0]-minHeight][bricks[0][1]-minWidth] = bricks[1]
+
+	#Get the height and width of the piece to use to construct its matrix representation
+	def getDimensions(self):
+		minHeight = self.brickList[0][0][0]
+		maxHeight = self.brickList[0][0][0]
+		minWidth = self.brickList[0][0][1]
+		maxWidth = self.brickList[0][0][1]
+
+		for bricks in self.brickList:
+			if  bricks[0][0] < minHeight:
+				minHeight = bricks[0][0]
+			if  bricks[0][0] > maxHeight:
+				maxHeight = bricks[0][0]
+			if  bricks[0][1] < minWidth:
+				minWidth = bricks[0][1]
+			if  bricks[0][1] > maxWidth:
+				maxWidth = bricks[0][1]
+
+		self.width = maxWidth - minWidth + 1
+		self.height = maxHeight - minHeight + 1
+
+		#Return the width/height so we know how big the array should be
+		#minheight/minwidth so we can translate brick locations from inputArray to locations in their small matrix.
+		return [minWidth, minHeight]
