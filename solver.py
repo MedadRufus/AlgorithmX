e685e12ca9c7994b067e9ccdc82a16ef2abad7df
Nathan Looney <looney@arqspin.com>
2015-12-10 13:58:32 -0500

    Forgot to pull before changing solver to check piece positions

--- solver.py
+++ solver.py
@@ -16,4 +16,34 @@ class Solver:
 	# def getPossiblePlacements(piece):
 	# 	for i in range()
 
-	def rotateNinetyDegrees(self, piece)
+	def getAllPositions(self, pieceList, puzzle):
+		for piece in pieceList:
+			for i in range(0,4):
+				self.shiftPiece(piece, puzzle)
+				piece.rotateClockwise()
+
+
+	def shiftPiece(self, piece, puzzle):
+		for i in range(0, puzzle.height-piece.height+1):
+			for j in range(0, puzzle.width-piece.width+1):
+				fits = self.checkFit(piece, puzzle, i, j)
+				if fits == True:
+					for brick in piece.brickList:
+						print brick[0][0] + i,
+						print " - ",
+						print brick[0][1] + j
+						print "\n"
+
+
+	def checkFit(self, piece, puzzle, posY, posX):
+		fits = True
+		for i in range(0, piece.height):
+			for j in range(0, piece.width):
+				try:
+					if piece.matrix[i][j] in puzzle.matrix[posY+i][posX+j]:
+						continue
+					else:
+						return False
+				except:
+					return False
+		return fits
